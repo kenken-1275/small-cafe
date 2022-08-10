b69825c6bb4b5f686047cbca3f9afffcf2187fd0
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-10 18:35:12 +0900

    管理者機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 root to: 'shops#index' 
+namespace :admin do
+  resources :announces,only:[:index,:new,:create]
+end
 resources :shops ,only: [:index,:show]
 
 end
