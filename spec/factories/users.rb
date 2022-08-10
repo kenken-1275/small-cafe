4730ab67f4d3d53405155e273a92ab688a3bffda
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-10 17:41:46 +0900

    FactoryBotとFaker導入、定義

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.initials(number: 3) }
+    email { Faker::Internet.free_email }
+    password { '7a' + Faker::Internet.password(min_length: 6, mix_case: false) }
+    password_confirmation { password }
+    kanji_last_name { '山田' }
+    kanji_first_name { '太郎' }
+    hiragana_last_name { 'やまだ' }
+    hiragana_first_name { 'たろう' }
+  end
+end
