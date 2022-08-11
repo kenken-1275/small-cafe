56240fdca09e1493c9fffabe454fbe238689a729
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-11 12:25:19 +0900

    お知らせ新規投稿機能実装

--- app/controllers/admin/announces_controller.rb
+++ app/controllers/admin/announces_controller.rb
@@ -2,12 +2,20 @@ class Admin::AnnouncesController < ApplicationController
   before_action :check_admin?
 
   def index
+    @announces = Announce.all
   end
 
   def new
+    @announce = Announce.new
   end
 
   def create
+    @announce = Announce.new(announce_params)
+    if @announce.save
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
   
 
@@ -18,4 +26,9 @@ class Admin::AnnouncesController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def announce_params
+    params.require(:announce).permit(:date,:title,:content)
+  end
+
 end
