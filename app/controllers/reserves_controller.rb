e943aff285f9419402adb47ce5150bc1d3e898b9
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-12 11:44:14 +0900

    reservesコントローラーcreateアクション定義

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -8,6 +8,17 @@ class ReservesController < ApplicationController
   end
 
   def create
+    @reserve = Reserve.new(reserve_params)
+    if @reserve.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private
+  def reserve_params
+    params.require(:reserve).permit(:date,:time_id,:people_number_id,:tel_number).merge(user_id:current_user.id)
   end
 
 end
--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -10,7 +10,6 @@ class Reserve < ApplicationRecord
   with_options presence: true do
     validates :tel_number,format:{with:/\A[0-9]{10,11}\z/,message: "is invalid."}
   end
-  validates :date,presence: true
 
   belongs_to :user
 
