59d4312369b7e3847877171ffd0a3e47f65ca429
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-08-15 12:50:46 +0900

    reserveモデル変更

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -13,4 +13,33 @@ class Reserve < ApplicationRecord
 
   belongs_to :user
 
+  OPEN_TIME = "11:00"
+  CLOSE_TIME = "16:00"
+  PER_TIME = 60.minutes
+
+  MIN_PEOPLE_NUMBER = 1
+  MAX_PEOPLE_NUMBER = 3 
+
+  def self.time_option
+    increment = PER_TIME.to_i
+    from_time = Time.parse(OPEN_TIME).to_i
+    to_time   = Time.parse(CLOSE_TIME).to_i
+    from_time.step(to_time, increment).map { |m| Time.at(m).strftime('%H:%M') }
+  end
+
+  def time_option
+    increment = PER_TIME.to_i
+    from_time = Time.parse(OPEN_TIME).to_i
+    to_time   = Time.parse(CLOSE_TIME).to_i
+    from_time.step(to_time, increment).map { |m| Time.at(m).strftime('%H:%M') }
+  end
+
+  def people_number_option
+    (MIN_PEOPLE_NUMBER..MAX_PEOPLE_NUMBER).map{|v| v}
+  end
+
+  def perse_resavation_time
+    resavation_time.strftime('%H:%M')
+  end
+
 end
