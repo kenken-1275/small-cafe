16dc34bbba0196265fdfa4cbdf5835349c14108f
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-09-08 15:08:34 +0900

    store_holidayモデル単体テストコード作成

--- spec/factories/store_holidays.rb
+++ spec/factories/store_holidays.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :store_holiday do
-    
+    store_holiday{Faker::Date.in_date_period}
   end
 end
--- spec/models/store_holiday_spec.rb
+++ spec/models/store_holiday_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe StoreHoliday, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @store_holiday = FactoryBot.build(:store_holiday)
+  end
+
+  describe '店休日設定機能' do
+    context '店休日設定できる時' do
+      it '日付けが正しく入力出来ていれば登録できる' do
+        expect(@store_holiday).to be_valid
+      end
+    end
+    context '店休日設定できない時' do
+      it 'store_holidayが空では登録できない' do
+        @store_holiday.store_holiday = ""
+        @store_holiday.valid?
+        expect(@store_holiday.errors.full_messages).to include("店休日が入力されていません。")
+      end
+      it 'store_holidayが既に登録されている場合は登録できない' do
+        @store_holiday.save
+        ano_holiday = FactoryBot.build(:store_holiday)
+        ano_holiday.store_holiday = @store_holiday.store_holiday
+        ano_holiday.valid?
+        expect(ano_holiday.errors.full_messages).to include("店休日は既に使用されています。")
+      end
+    end
+  end
 end
