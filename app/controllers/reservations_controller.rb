41de48fe68e329afd47c5c6c46c66c09d3e02642
kenken-1275 <ich.liebe.r.h.u.t.1@gmail.com>
2023-03-22 15:23:34 +0900

    render2

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -69,11 +69,9 @@ class ReservationsController < ApplicationController
     reservations_total = Reservation.select("reservation_date,reservation_time,people_number").group(:reservation_date).group(:reservation_time).sum(:people_number)
     reservations_total_people_number = reservations_total.values
     i = 0
-    if @reservations.present?
-      @reservations.each do |reservation|
-        reservation[:people_number] = reservations_total_people_number[i]
-        i+=1
-      end
+    @reservations.each do |reservation|
+      reservation[:people_number] = reservations_total_people_number[i]
+      i+=1
     end
   end
 
